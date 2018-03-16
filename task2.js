98fee7875e5f704bde4534894dbfbe9fad150830
Ilya Nidchenk <zp@sd.ua>
2018-03-17 01:34:44 +0200

    edit task2

--- task2.js
+++ task2.js
@@ -1,8 +1,10 @@
 function deepEquals(array1, array2) {
-  for (var arrays = [], objects = {}, element = 0; element < array1.length; element++) objects[array1[element]]? objects[array1[element]]++:objects[array1[element]]=1 ;
-3
-    for (element = 0; element < array2.length; element++) objects[array2[element]] && arrays.push(array2[element]) && objects[array2[element]]--;
-4
+  for (var arrays = [], objects = {}, element = 0; element < array1.length; element++){ 
+  	objects[array1[element]]? objects[array1[element]]++:objects[array1[element]]=1 ;
+  }
+    for (element = 0; element < array2.length; element++) {
+    	objects[array2[element]] && arrays.push(array2[element]) && objects[array2[element]]--;
+}
     return arrays.length == array1.length && array2.length == array1.length
 
 }
